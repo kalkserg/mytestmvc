7bfbe825bd31bce337533868a930c59972ace8c9
kalkserg <kalkserg@ukr.net>
2021-05-26 16:57:59 +0300

    second

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -2,15 +2,21 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping(value = "/greeting")
-    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World!") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+//    @RequestMapping(value = "/greeting")
+//    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World!") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "greeting";
+//    }
+
+    @PostMapping(value = "/")
+    public String postBody(@RequestBody String str, Model model) {
+        //System.out.println(str);
+        model.addAttribute("str", str);
+        return "sample";
     }
 }
