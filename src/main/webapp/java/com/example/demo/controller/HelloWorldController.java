7d174ed5f28cdb8d3962b4185a2edefa66566bc4
kalkserg <kalkserg@ukr.net>
2021-05-26 18:09:05 +0300

    5

--- src/main/webapp/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/webapp/java/com/example/demo/controller/HelloWorldController.java
@@ -7,27 +7,30 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HelloWorldController {
 
+    String text ="";
 //    @RequestMapping(value = "/greeting")
 //    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World!") String name, Model model) {
 //        model.addAttribute("name", name);
 //        return "greeting";
 //    }
 
-    @RequestMapping()
-//    @PostMapping()
+//    @RequestMapping()
+    @PostMapping()
     public String postBody(@RequestBody(required = false) String str, Model model) {
         //System.out.println(str);
-        if (str!=null) model.addAttribute("str", str);
+
+        if (str!=null) { text = text + str + "\n";model.addAttribute("str", text);}
         else model.addAttribute("str", "EMPTY");
         return "sample";
     }
 
-//    @GetMapping()
-//    public String getBody(@RequestBody(required = false) String str, Model model) {
-//        System.out.println(str);
-//        if (str!=null) model.addAttribute("str", str);
-//        else model.addAttribute("str", "EMPTY");
-//        return "sample";
-//    }
+    //{"device" : "{device}","time" : "{time}","data" : "{data}","seqNumber" : "{seqNumber}","lqi" : "{lqi}","operatorName" : "{operatorName}"}
+
+    @GetMapping()
+    public String getBody(@RequestBody(required = false) String str, Model model) {
+        //System.out.println(str);
+        model.addAttribute("str", text);
+        return "byby";
+    }
 
 }
