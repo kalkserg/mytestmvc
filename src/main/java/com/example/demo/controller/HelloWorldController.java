4941bf379a372bfa4617dae771759001289f8cbc
kalkserg <kalkserg@ukr.net>
2021-07-07 14:40:52 +0300

    url

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,36 +1,48 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
 
     String text ="";
+    private String UrlResource;
 //    @RequestMapping(value = "/greeting")
 //    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World!") String name, Model model) {
 //        model.addAttribute("name", name);
 //        return "greeting";
 //    }
 
-//    @RequestMapping()
+    //    @RequestMapping()
     @PostMapping()
-    public String postBody(@RequestBody(required = false) String str, Model model) {
-        //System.out.println(str);
+    public ResponseEntity<String> postBody(@RequestBody String str, HttpServletRequest request) {
+        System.out.println(str);
 
-        if (str!=null) { text = text + str + "\n";model.addAttribute("str", text);}
-        else model.addAttribute("str", "EMPTY");
-        return "sample";
+        String baseUrl = ServletUriComponentsBuilder.fromRequestUri(request)
+                .replacePath(null)
+                .build()
+                .toUriString();
+
+        System.out.println(baseUrl);
+
+        //if (str!=null) { text = text + str + "\n";model.addAttribute("str", text);}
+        //else model.addAttribute("str", "EMPTY");
+        //return "sample";
+        return new ResponseEntity<String>("Ok", HttpStatus.OK);
     }
 
     //{"device" : "{device}","time" : "{time}","data" : "{data}","seqNumber" : "{seqNumber}","lqi" : "{lqi}","operatorName" : "{operatorName}"}
 
-    @GetMapping()
-    public String getBody(@RequestBody(required = false) String str, Model model) {
-        //System.out.println(str);
-        model.addAttribute("str", text);
-        return "byby";
-    }
 
 }
