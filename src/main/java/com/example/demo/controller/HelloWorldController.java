870f64d0fc5f85b6675d75108f8fc46e306d38e5
kalkserg <kalkserg@ukr.net>
2021-07-08 12:34:57 +0300

    18

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,39 +12,46 @@ import javax.servlet.http.HttpServletRequest;
 public class HelloWorldController {
 
     String text ="";
-    @RequestMapping(value = "/greeting")
-    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World!") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
+
+//    @RequestMapping(value = "/greeting")
+//    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World!") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "greeting";
+//    }
 
 //        @RequestMapping()
     @PostMapping()
     public ResponseEntity<String> postBody(@RequestBody(required = false)  String str, HttpServletRequest request, Model model) {
 //    public String postBody(@RequestBody(required = false)  String str, HttpServletRequest request) {
-        //System.out.println(str);
+        System.out.println("POST "+str);
+//        String text ="";
 
-        String ref = request.getRequestURI();
-        String baseUrl = ServletUriComponentsBuilder.fromRequestUri(request)
-                .replacePath(null)
-                .build()
-                .toUriString();
+        StringBuffer ref = request.getRequestURL();
+//        String baseUrl = ServletUriComponentsBuilder.fromRequestUri(request)
+//                .replacePath(null)
+//                .build()
+//                .toUriString();
 
-
+            text = "POST " + str + " " + " " + ref + "\n";
 //        System.out.println(baseUrl);
 
-        if (str!=null) { text = text + str + " " + baseUrl + " " + ref + "\n"; model.addAttribute("str", text);}
-        else model.addAttribute("str", "EMPTY");
-        //return "sample";
-        return new ResponseEntity<String>("Ok", HttpStatus.OK);
+//        if (str!=null) { text = text + str + " " + baseUrl + " " + ref + "\n"; model.addAttribute("str", text);}
+//        else
+            model.addAttribute("str", text);
+
+        String tmp = "{\"device\" : \"01F2395A\",\"data\" : \"0101000000000000\"}";
+//        model.addAttribute("str", tmp);
+//        return "sample";
+        return new ResponseEntity<String>(tmp, HttpStatus.OK);
     }
 
     //{"device" : "{device}","time" : "{time}","data" : "{data}","seqNumber" : "{seqNumber}","lqi" : "{lqi}","operatorName" : "{operatorName}"}
 
     @GetMapping()
-    public String getBody(@RequestBody(required = false) String str, Model model) {
-        //System.out.println(str);
+    public String getBody(@RequestBody(required = false)  String str,Model model) {
+        System.out.println("GET "+str);
 //        model.addAttribute("str", str);
+        text = text + "GET " + str + "\n";
         model.addAttribute("str", text);
         return "byby";
     }
