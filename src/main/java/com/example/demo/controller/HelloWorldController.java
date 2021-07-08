e84c8e38d095298194d1e401a25fd592ce20e966
kalkserg <kalkserg@ukr.net>
2021-07-08 14:22:30 +0300

    23

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 public class HelloWorldController {
@@ -21,7 +22,7 @@ public class HelloWorldController {
 
 //        @RequestMapping()
     @PostMapping()
-    public String postBody(@RequestBody(required = false)  String str, HttpServletRequest request, HttpServletResponse response, Model model) throws IOException {
+    public void postBody(@RequestBody(required = false)  String str, HttpServletRequest request, HttpServletResponse response, Model model) throws IOException {
 //    public String postBody(@RequestBody(required = false)  String str, HttpServletRequest request) {
         System.out.println("POST "+str);
 //        String text ="";
@@ -41,7 +42,12 @@ public class HelloWorldController {
 
         String tmp = "{\"device\" : \"1F2395A\",\"downlinkData\" : \"0101000000000000\"}";
         model.addAttribute("str", tmp);
-        return "sample";
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(tmp);
+        out.flush();
+        //return "sample";
 //        response.setContentType("application/json");
 //        response.setCharacterEncoding("UTF-8");
 //        response.getWriter().write(tmp);
