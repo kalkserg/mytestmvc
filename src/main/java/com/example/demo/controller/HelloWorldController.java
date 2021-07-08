6da7272d4c58d06bd3a6e90b98b47b334675a69a
kalkserg <kalkserg@ukr.net>
2021-07-08 14:26:12 +0300

    24

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -40,7 +40,7 @@ public class HelloWorldController {
 //        else
             model.addAttribute("str", text);
 
-        String tmp = "{\"device\" : \"1F2395A\",\"downlinkData\" : \"0101000000000000\"}";
+        String tmp = "{\"device\" : \"1F2395A\",\"data\" : \"0101000000000000\"}";
         model.addAttribute("str", tmp);
         PrintWriter out = response.getWriter();
         response.setContentType("application/json");
